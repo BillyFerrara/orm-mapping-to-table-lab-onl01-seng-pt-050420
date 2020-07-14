ac8c71aafa9a568ea6c8dee81314bbf5baebd377
Billy Ferrara <willfira717@gmail.com>
2020-07-14 18:38:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,8 +40,10 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create
-    
+  def self.create(name, grade)
+    song = Song.new(name, grade)
+    song.save 
+    song 
   end
 
 end
