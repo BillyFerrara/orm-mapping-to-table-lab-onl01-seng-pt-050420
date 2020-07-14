f6f0a058228c1f0ec47864b9425333b462847160
Billy Ferrara <willfira717@gmail.com>
2020-07-14 18:32:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,6 +35,7 @@ class Student
       INSERT INTO students (name, grade)
       VALUES (?, ?)
       DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students")[0][0]
     SQL
   end
 
