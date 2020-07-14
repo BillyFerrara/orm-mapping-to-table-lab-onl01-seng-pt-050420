39604a278a7b0f99adabad38072169c9c5ddad9e
Billy Ferrara <willfira717@gmail.com>
2020-07-14 18:20:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,12 +4,24 @@ class Student
   #  with DB[:conn]
 
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
 
   def initialize(name, grade, id=nil)
     @id = id
     @name = name
     @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+      DB[:conn].execute(sql)
+      end 
   end
 
 
