bf714f5b4bc55861692fa0ad8d2d3e51607fca03
Billy Ferrara <willfira717@gmail.com>
2020-07-14 18:24:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,8 +18,16 @@ class Student
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade TEXT
-      )
+      );
       SQL
       DB[:conn].execute(sql)
   end
+
+  def self.drop_table
+    sql = <<- SQL
+    drop table stuents;
+    SQL
+    DB[:conn].execute(sql)
+  end
+
 end
